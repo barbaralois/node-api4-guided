e3c288cef0cc291e93754db91a58d482ed891418
Barbara Moore <barbaraloismoore@gmail.com>
2020-07-09 12:47:59 -0400

    ready to deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require("./api/server.js");
+const server = require('./api/server.js');
 
-const port = 5000;
+const port = process.env.PORT || 5000; // making the PORT dynamic, if that env variable is not there then it will use 5000
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
